cc9297751b537734d6ecaae03a35cd299dd089fe
FedericoMS <63051263+FedericoMS@users.noreply.github.com>
2020-05-15 23:57:28 -0300

    Add files via upload

---
+++ TP Nº2 Empresa/auxiliarFunctions.h
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <string.h>
+
+int getInt(char message[], char errorMessage[], int min, int max);
+/** \brief It returns a validated int
+ *
+ * \param message[] char
+ * \param errorMessage[] char
+ * \param min int
+ * \param max int
+ * \return int
+ *
+ */
+
+float getFloat(char message[], char errorMessage[], int min, int max);
+/** \brief it returns a validated float
+ *
+ * \param message[] char
+ * \param errorMessage[] char
+ * \param min int
+ * \param max int
+ * \return float
+ *
+ */
+
+char getLetter(char message[], char errorMessage[]);
+/** \brief it returns a validated letter
+ *
+ * \param message[] char
+ * \param errorMessage[] char
+ * \return char
+ *
+ */
+
+void getString(char message[], char errorMessage[], char string[]);
+/** \brief it returns a validated string
+ *
+ * \param message[] char
+ * \param errorMessage[] char
+ * \param str[] char
+ * \return void
+ *
+ */
+
---
+++ TP Nº2 Empresa/main.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "auxiliarFunctions.h"
+#include "ArrayEmployees.h"
+#define TamEmpresa 1000
+
+int main()
+{
+    Employee EmployeeArray[TamEmpresa];
+    initEmployees(EmployeeArray, TamEmpresa);
+    char opcion;
+    do{
+    opcion = menu();
+    switch(opcion)
+    {
+        case 'a':
+            createAnEmployee(EmployeeArray, TamEmpresa);
+            fflush(stdin);
+            break;
+
+        case 'b':
+            modifyEmployee(EmployeeArray, TamEmpresa);
+            fflush(stdin);
+            break;
+        case 'c':
+            findAnEmployeeToDelete(EmployeeArray, TamEmpresa);
+            fflush(stdin);
+            break;
+        case 'd':
+            reportInformation(EmployeeArray, TamEmpresa);
+            fflush(stdin);
+            break;
+        case 'e':
+            printf("Preparando para cerrar\n");
+            break;
+       default:
+            printf("Opcion incorrecta\n");
+            fflush(stdin);
+            break;
+
+    }
+
+    system("pause");
+    system("cls");
+}while(opcion != 'e');
+
+}
+
